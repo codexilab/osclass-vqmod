465620ca9752baa5b5a21386e367192342b78913
AdrianOlmedo <adrianolmedo.ve@gmail.com>
2020-02-12 18:14:38 -0400

    Added ABS_PATH control

--- vqmod/views/admin/file_source_iframe.php
+++ vqmod/views/admin/file_source_iframe.php
@@ -1,4 +1,5 @@
-<?php
+<?php if ( ! defined('ABS_PATH')) exit('ABS_PATH is not loaded. Direct access is not allowed.');
+
 /**
  * @author Adrián Olmedo <adrianolmedo.ve@gmail.com>
  * @copyright (c) 2020 CodexiLab
